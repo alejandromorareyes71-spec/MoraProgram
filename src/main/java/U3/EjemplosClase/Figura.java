9dcafb44289088b3c6642e9c5c7d923e3699be45
Alejandro Mora Reyes <234221926+alejandromorareyes71-spec@users.noreply.github.com>
2025-11-25 12:48:37 +0100

    ClaseFigura

--- src/main/java/U3/EjemplosClase/Figura.java
+++ src/main/java/U3/EjemplosClase/Figura.java
@@ -1,14 +1,27 @@
 package U3.EjemplosClase;
 
 public class Figura {
-    private  int num_lados;
-    private  String color;
-   public static void main(String[] args) {
-    public void getFigura{
-
-       }
-       public void setFigura{}
-
-
+    private int numLados;
+    private String color;
+    public Figura (int numLados, String color){
+        setNumLados(numLados);
+        setColor(color);
     }
+
+        public int getNumLados() {
+        return numLados;
+    }
+
+    public void setNumLados(int numLados) {
+        this.numLados = numLados;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
 }
