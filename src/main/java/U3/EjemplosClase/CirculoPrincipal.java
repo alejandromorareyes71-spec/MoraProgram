ce592e9189fd99a45dc9c4d82edb367bc0859583
Alejandro Mora Reyes <234221926+alejandromorareyes71-spec@users.noreply.github.com>
2025-12-02 12:46:34 +0100

    Delete src/main/java/U3/EjemplosClase/CirculoPrincipal.java

--- src/main/java/U3/EjemplosClase/CirculoPrincipal.java
+++
@@ -1,9 +0,0 @@
-package U3.EjemplosClase;
-
-public class CirculoPrincipal {
-    public static void main(String[] args) {
-        Circulo c1= new Circulo(1000.20);
-        c1.setRadio(7);
-
-    }
-}
