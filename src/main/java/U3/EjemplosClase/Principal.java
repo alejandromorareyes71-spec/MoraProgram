a1cfa2ee0f936ee613c20061df437eea37573d49
Alejandro Mora Reyes <234221926+alejandromorareyes71-spec@users.noreply.github.com>
2025-12-02 12:47:24 +0100

    Delete src/main/java/U3/EjemplosClase/Principal.java

--- src/main/java/U3/EjemplosClase/Principal.java
+++
@@ -1,9 +0,0 @@
-package U3.EjemplosClase;
-
-public class Principal {
-    public static void main(String[] args) {
-        Persona persona = new Persona("Avelino", 30);
-        persona.saludar();
-    }
-}
-
