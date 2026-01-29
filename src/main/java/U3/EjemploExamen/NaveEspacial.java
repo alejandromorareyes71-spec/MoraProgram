76129b84488b70fd1bb2c3baab68e640b02df801
alejandromorareyes71-spec <alejandromorareyes71@gmail.com>
2026-01-29 08:33:44 +0100

    Create NaveEspacial.java

---
+++ src/main/java/U3/EjemploExamen/NaveEspacial.java
@@ -0,0 +1,51 @@
+package U3.EjemploExamen;
+
+public class NaveEspacial {
+    String nombre;
+    int combustible;
+    boolean enMision;
+    public NaveEspacial(String nombre, int combustible, boolean enMision){
+        this.nombre=nombre;
+        this.combustible=combustible;
+        this.enMision=enMision;
+    }
+    //Getter y Setter
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCombustible() {
+        return combustible;
+    }
+
+    public void setCombustible(int combustible) {
+        this.combustible = combustible;
+    }
+
+    public boolean isEnMision() {
+        return enMision;
+    }
+
+    public void setEnMision(boolean enMision) {
+        this.enMision = enMision;
+    }
+    //Metodos
+    public void despegar() {
+        if (getCombustible() >= 100) {
+            this.enMision = true;
+        } else {
+            System.out.println("No hay suficiente combustible");
+        }
+    }
+        public void aterrizar(){
+            this.enMision=false;
+        }
+        public void consumirCombustible(){}
+    }
+
+
+}
